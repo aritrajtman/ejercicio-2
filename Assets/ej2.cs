65b8e58977032431c5b57885b0fb49ff61465500
aritrajtman <47066506@est.ort.edu.ar>
2022-04-22 03:53:19 -0300

    Initial commit

---
+++ Assets/ej2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ej2 : MonoBehaviour
+{
+    public Text MiTexto;
+    public int EdadUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (EdadUsuario >= 18)
+        {
+            MiTexto.text = "El usuario es mayor de edad";
+            Debug.Log("El usuario es mayor de edad");
+        }
+        else
+        {
+            MiTexto.text = "El usuario es menor de edad";
+            Debug.Log("El usuario es menor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# mayoria-de-edad
+ 
