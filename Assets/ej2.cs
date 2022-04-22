85465df0c39e3d8da7bc125ba9b3a03f4fc1167a
aritrajtman <47066506@est.ort.edu.ar>
2022-04-22 04:02:58 -0300

    mayor de edad terminado

--- Assets/ej2.cs
+++ Assets/ej2.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class ej2 : MonoBehaviour
 {
-    public Text MiTexto;
     public int EdadUsuario;
+    public Text MiTexto;
+    
     // Start is called before the first frame update
     void Start()
     {
